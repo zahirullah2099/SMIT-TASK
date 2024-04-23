cf431cf28a5b59da58057dee1ea972648d1f0284
zahir-ullah <zahirullah2099@gmail.com>
2024-04-23 11:00:39 +0500

    update version
    update version

---
+++ script.js
@@ -0,0 +1,23 @@
+ document.getElementById("student-data").addEventListener('submit',(Event) => {
+    Event.preventDefault();
+    const rollNo = document.getElementById("roll-no").value;
+    const name = document.getElementById("name").value;
+ const batch= document.getElementById("batch").value;
+ const section = document.getElementById("section").value;
+
+ const data={
+     rollNo : rollNo,
+    Name : name,
+    Batch : batch,
+    Section : section
+ };
+ 
+ const actualData = JSON.stringify(data);
+ console.log(actualData);
+sessionStorage.setItem("studentDetails" , actualData);
+alert("student details has been stored in session");
+
+document.getElementById("student-data").reset();
+
+});
+ 
